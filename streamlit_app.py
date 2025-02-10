877d167ed560d751f89e40f2fea0a3c74d0805ea
DataAnalystVR <vyacheslavrusanov@gmail.com>
2025-02-10 16:50:28 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,6 +42,6 @@ if ingredients_list:
 # New request to display smoothiefroot information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
 
 
